f690e83ecc27d788ea8edf2f43038ef4f1b54800
Marketa <markettka@email.cz>
2019-04-07 23:02:05 +0200

    add new record

--- app.js
+++ app.js
@@ -1,4 +1,64 @@
-var budgetController = (function()  {}) ();
+// ----------------- Budget Controller ----------------------------- //
+var budgetController = (function()  {
+   
+  // constructor of type Expense
+  var Expense = function(id,desc, value) {
+    this.id = id;
+    this.desc = desc;
+    this.value = value; 
+  }
+
+  // constructor of type Income
+  var Income = function (id, desc, value) {
+    this.id = id;
+    this.desc = desc;
+    this.value = value;
+  }
+
+  // incomes & expenses data structure
+  var data = {
+    allRecords: {
+      exp: [],
+      inc: [] 
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+  return {
+    // add new Expense or Income
+    addRecord: function(type,des,val) {
+      var newRecord, ID;
+
+      // assign ID
+      if(data.allRecords[type].length > 0) {
+        ID = data.allRecords[type][data.allRecords[type].length-1].id + 1
+      }
+      else {
+        ID = 0
+      }
+      
+      // determine exp or inc
+      if(type === 'exp') {
+        newRecord = new Expense(ID, des, val);
+      }
+      else if(type === 'inc') {
+        newRecord = new Income(ID, des, val)
+      }
+      
+      // push to the correct data array
+      data.allRecords[type].push(newRecord);
+      return newRecord;
+    },
+
+    testing: function () {
+      console.log(data);
+    }
+  }
+
+}) ();
 
 
 //--------------------- UI Controller ---------------------------------------//
@@ -51,9 +111,14 @@ var appController = (function(budgetCtrl, UICtrl) {
   }
   
   var addItem = function() {
+    var input;
+    var record;
+
     // 1. Get the field input data
-    var input = UICtrl.getInput()
-    console.log(input);
+    input = UICtrl.getInput();
+    
+    // 2. Add record to the budgetController
+    record = budgetCtrl.addRecord(input.type, input.decription, input.value)
   }
   
   return {
