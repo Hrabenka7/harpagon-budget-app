f445df15061d0d8d9f0cb29694e09cbf0aef27e5
Marketa <markettka@email.cz>
2019-04-10 21:09:50 +0200

    expenses/income list

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ var UIController = (function () {
     type: '.add__type',
     description: '.add__description',
     value: '.add__value',
-    button: '.add__btn'
+    button: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list'
   };
   
   
@@ -78,7 +80,7 @@ var UIController = (function () {
     getInput: function () {
       return {
         type : document.querySelector(DOMstrings.type).value,
-        decription : document.querySelector(DOMstrings.description).value,
+        description : document.querySelector(DOMstrings.description).value,
         value : document.querySelector(DOMstrings.value).value
       };
     },
@@ -86,6 +88,47 @@ var UIController = (function () {
     // get DOM elements names
     getDOMstrings: function () {
       return DOMstrings;
+    },
+
+   // add new record into the list
+    addListRecord: function (obj, type) {
+      var html, newHtml, element;
+
+      if(type === 'inc') 
+      {
+        element = DOMstrings.incomeContainer
+
+        html = ` <div class="item clearfix" id="income-%id%">
+                    <div class="item__description">%description%</div>
+                    <div class="right clearfix">
+                        <div class="item__value">%value%</div>
+                        <div class="item__delete">
+                            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        </div>
+                    </div>
+                </div>`
+      }
+      else if(type === 'exp') 
+      {
+        element = DOMstrings.expenseContainer
+
+        html = `<div class="item clearfix" id="expense-%id%">
+          <div class="item__description">%description%</div>
+          <div class="right clearfix">
+            <div class="item__value">%value%</div>
+            <div class="item__percentage">21%</div>
+            <div class="item__delete">
+              <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+            </div>
+          </div>
+        </div>`
+      }      
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.desc);
+      newHtml = newHtml.replace('%value%', obj.value)
+    
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
     }
   };
   
@@ -101,24 +144,29 @@ var appController = (function(budgetCtrl, UICtrl) {
     var DOM = UICtrl.getDOMstrings()
     
     // event listeners for button ADD (tick)
-    document.querySelector(DOM.button).addEventListener('click', addItem); 
+    document.querySelector(DOM.button).addEventListener('click', addRecord); 
     document.addEventListener('keypress', function(event){
       if(event.keyCode === 13 || event.which === 13) {  // enter key pressed
-        addItem();
+        addRecord();
       } 
     });
     
   }
   
-  var addItem = function() {
+  var addRecord = function() {
     var input;
     var record;
 
     // 1. Get the field input data
     input = UICtrl.getInput();
+    console.log('input', input)
     
     // 2. Add record to the budgetController
-    record = budgetCtrl.addRecord(input.type, input.decription, input.value)
+    record = budgetCtrl.addRecord(input.type, input.description, input.value)
+  
+    // 3. Add record to the UI
+    UIController.addListRecord(record, input.type)
+  
   }
   
   return {
